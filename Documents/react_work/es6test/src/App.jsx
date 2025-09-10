e53b1759b36ce2e33a3fbbfe1fc71d383bc2c8d7
이재준 <jeajune11117@gmail.com>
2025-09-10 17:41:17 +0900

    연습

---
+++ Documents/react_work/es6test/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import TodoList from './TodoList'
+function App() {
+
+  return (
+    <>
+      <h1>Hello React World</h1>
+      <TodoList/>
+    </>
+  )
+}
+
+export default App
